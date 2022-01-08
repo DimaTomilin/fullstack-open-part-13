fd2dac2b45123f73764ba043ce3db6cb88ea8625
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2022-01-08 15:02:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const { Sequelize, Model, DataTypes, QueryTypes} = require('sequelize')
+const express = require('express')
+const app = express()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+})
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.STRING
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+app.get('/api/blogs', async (req, res) => {
+    console.log("1");
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    res.json(blogs)
+  })
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
